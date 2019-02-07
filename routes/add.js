d54ca5d896c1b97538c4b82d6e3fd3c8110f3576
evonhung <46575107+evonhung@users.noreply.github.com>
2019-02-07 13:41:35 -0800

    Add files via upload

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,16 @@
 var data = require("../data.json");
 
 exports.addFriend = function(request, response) {    
-	// Your code goes here
- }
+	var nameFriend = request.query.name;
+	var descFriend = request.query.description;
+
+	var newFriend = {
+		"name": nameFriend,
+		"description": descFriend,
+		"imageURL": "http://lorempixel.com/400/400/people",
+	};
+
+	data.friends.push(newFriend);
+		console.log(newFriend);
+		response.render("index.handlebars", data);
+ };
